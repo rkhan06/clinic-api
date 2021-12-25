8ce127fac34371985b6d6c713c7755aea7677128
Rameen <rameen07@gmail.com>
2021-12-25 18:49:00 +0500

    test fix

--- test/clinic.js
+++ test/clinic.js
@@ -5,25 +5,21 @@ const server = require('../app');
 
 // assertion type
 var should = chai.should();
-var expect = chai.expect;
 
 chai.use(chaiHTTP);
 
 describe('Clinics API', () => {
   // Test the GET Route
   describe('GET /api/clinic', () => {
-    it('should return all dental and vet clinics', (done) => {
-      chai.request(server)
-        .get('/api/clinic')
-        .end((err, res) => {
-          should.not.exist(err);
-          should.exist(res);
-          res.should.have.status(200);
-          res.body.should.be.an('Object');
-          res.body.success.should.be.an('Boolean');
-          res.body.should.have.property('data').with.lengthOf(2);
-          done();
-        })
+    it('should return all dental and vet clinics', async () => {
+      const res = await chai.request(server).get('/api/clinic');
+      should.exist(res);
+      res.should.have.status(200);
+      res.body.should.be.an('Object');
+      res.body.success.should.be.an('Boolean');
+      res.body.should.have.property('data').which.should.be.an('Object');
+      res.body.data.should.have.property('dentalClinics').that.is.an('Array');
+      res.body.data.should.have.property('vetClinics').that.is.an('Array');
     })
   })
 })
